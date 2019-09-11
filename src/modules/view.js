077cb1b44f133c61426576bc6683079f8b719707
Duc Doan <ducdhm@gmail.com>
2019-09-11 13:42:17 +0700

    Fix view module

--- src/modules/view.js
+++ src/modules/view.js
@@ -1,13 +1,11 @@
 // View config
 // --------------------------------
 const resolvePath = require('../utils/resolvePath');
-const expressEdge = require('express-edge');
+const {config: edgeConfig, engine} = require('express-edge');
 module.exports = (app, config) => {
-    app.use(expressEdge);
+    app.use(engine);
     app.set('views', resolvePath(app.id, 'views'));
 
     // View cache for production
-    if (config.view.cache) {
-        app.enable('view cache');
-    }
+    edgeConfig({cache: config.view.cache});
 };
