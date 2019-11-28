af7a891c1ffa02220385431e0134668115ff83fc
Duc Doan <ducdhm@gmail.com>
2019-11-28 14:22:41 +0700

    Upgrade `meen-utils` version and gen `modules/index.js` file tool

---
+++ tools/genModuleIndex.js
@@ -0,0 +1,12 @@
+const { getFileList, resolvePath } = require('meen-utils');
+const fs = require('fs');
+const path = require('path');
+const Handlebars = require('handlebars');
+
+const fileList = getFileList(resolvePath('src', 'modules')).map(item => path.parse(item).name).filter(item => item !== 'index');
+const template = fs.readFileSync(resolvePath('tools', 'templateModuleIndex.hbs'), 'utf8');
+const content = Handlebars.compile(template)({
+    utils: fileList,
+});
+
+fs.writeFileSync(resolvePath('src', 'modules', 'index.js'), content);
