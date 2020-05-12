d745ce2cd7b21b5dec4d5d80ce24508e005d1e7f
Duc Doan <ducdhm@gmail.com>
2020-05-12 23:17:17 +0700

    v1.7.0

--- tools/publishNewVersion/index.js
+++ tools/publishNewVersion/index.js
@@ -8,28 +8,28 @@ const logger = getLogger(true);
 
 (() => {
     try {
-        logger.info(`-> Current version: "${currentVersion}" - new version: "${newVersion}"`);
+        logger.info(`Current version: "${currentVersion}" - new version: "${newVersion}"`);
 
-        logger.info(`-> Update version: "${newVersion}" to "package.json"`);
+        logger.info(`Update version: "${newVersion}" to "package.json"`);
         packageJson.version = newVersion;
         fs.writeFileSync('./package.json', JSON.stringify(packageJson, ' ', 4), 'utf-8');
 
-        logger.info(`-> Generate "modules/index.js" file`);
+        logger.info(`Generate "modules/index.js" file`);
         execSync(`node tools/genModuleIndex`);
 
-        logger.info(`-> Push to github with message "v${newVersion}"`);
+        logger.info(`Push to github with message "v${newVersion}"`);
         execSync(`git add .`);
         execSync(`git commit -m "v${newVersion}"`);
         execSync(`git push origin master`);
 
-        logger.info(`-> Create new release with message "Release of version ${newVersion}"`);
+        logger.info(`Create new release with message "Release of version ${newVersion}"`);
         execSync(`git tag -a v${newVersion} -m "Release of v${newVersion}"`);
         execSync('git push --tags');
 
-        logger.info(`-> Publish version "${newVersion}" to "https://www.npmjs.com/"`);
+        logger.info(`Publish version "${newVersion}" to "https://www.npmjs.com/"`);
         execSync('npm publish');
 
-        logger.info(`-> "${newVersion}" is published!`);
+        logger.info(`"${newVersion}" is published!`);
     } catch (e) {
         logger.error(`${e}`);
     }
