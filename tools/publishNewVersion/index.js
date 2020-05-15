632158d055d3a0cc9486dfb4578167f8a4ba2b95
Duc Doan <ducdhm@gmail.com>
2020-05-15 17:00:05 +0700

    v1.7.3

--- tools/publishNewVersion/index.js
+++ tools/publishNewVersion/index.js
@@ -1,36 +1,7 @@
-const fs = require('fs');
-const packageJson = require('../../package');
-const currentVersion = packageJson.version;
-const { increaseVersionNumber, getLogger } = require('@meenjs/utils');
-const newVersion = increaseVersionNumber(currentVersion);
+const { publishNewVersion } = require('@meenjs/utils');
 const { execSync } = require('child_process');
-const logger = getLogger(true);
 
-(() => {
-    try {
-        logger.info(`Current version: "${currentVersion}" - new version: "${newVersion}"`);
-
-        logger.info(`Update version: "${newVersion}" to "package.json"`);
-        packageJson.version = newVersion;
-        fs.writeFileSync('./package.json', JSON.stringify(packageJson, ' ', 4), 'utf-8');
-
-        logger.info(`Generate "modules/index.js" file`);
-        execSync(`node tools/genModuleIndex`);
-
-        logger.info(`Push to github with message "v${newVersion}"`);
-        execSync(`git add .`);
-        execSync(`git commit -m "v${newVersion}"`);
-        execSync(`git push origin master`);
-
-        logger.info(`Create new release with message "Release of version ${newVersion}"`);
-        execSync(`git tag -a v${newVersion} -m "Release of v${newVersion}"`);
-        execSync('git push --tags');
-
-        logger.info(`Publish version "${newVersion}" to "https://www.npmjs.com/"`);
-        execSync('npm publish');
-
-        logger.info(`"${newVersion}" is published!`);
-    } catch (e) {
-        logger.error(`${e}`);
-    }
-})();
+publishNewVersion(async (logger) => {
+    logger.info(`Generate "modules/index.js" file`);
+    execSync(`node tools/genModuleIndex`);
+});
