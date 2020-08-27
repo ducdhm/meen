f4d2c8b0c0486630c039ff79bc44270547144f41
Duc Doan <ducdhm@gmail.com>
2020-08-27 17:31:21 +0700

    God is god

--- src/composeApp/checkPermission.js
+++ src/composeApp/checkPermission.js
@@ -4,6 +4,10 @@ module.exports = (app, permission) => async (req, res, next) => {
     let { role, LOCALE } = app;
     let userPermission = role[req.user.role];
 
+    if (req.user.god) {
+        return next();
+    }
+
     if (userPermission.indexOf(permission) === -1) {
         return next(newError(403, LOCALE.PAGE_ERROR__ERROR_403));
     }
