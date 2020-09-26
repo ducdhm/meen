b6acac3489a8439a6b9bae1f97358623ac51f6b6
Duc Doan <ducdhm@gmail.com>
2020-09-26 20:33:59 +0700

    publicFolder support config as object

--- src/modules/publicFolder.js
+++ src/modules/publicFolder.js
@@ -4,18 +4,27 @@ const express = require('express');
 
 module.exports = (app, config) => {
     const logger = app.logger('STATIC');
+    const setupPublic = (publicUrl, publicFolderPath) => {
+        app.use(
+            publicUrl,
+            express.static(
+                publicFolderPath,
+                config.publicFolder.debug ? {
+                    index: false,
+                    setHeaders: (response, filePath) => {
+                        // Logging work
+                        logger.debug(`Serve "${response.req.originalUrl}" from "${filePath}"`);
+                    },
+                } : null,
+            ),
+        );
+    };
 
-    app.use(
-        '/public',
-        express.static(
-            config.publicFolder.path,
-            config.publicFolder.debug ? {
-                index: false,
-                setHeaders: (response, filePath) => {
-                    // Logging work
-                    logger.debug(`Serve "${response.req.originalUrl}" from "${filePath}"`);
-                },
-            } : null,
-        ),
-    );
+    if (typeof config.publicFolder.path === 'string') {
+        setupPublic('/public', config.publicFolder.path);
+    } else {
+        for (let url in config.publicFolder.path) {
+            setupPublic(url, config.publicFolder.path[url]);
+        }
+    }
 };
